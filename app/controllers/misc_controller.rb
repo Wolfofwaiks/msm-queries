7d33b53f209d41a8cc13f31d6ef225b9973f8b44
Wolfofwaiks <165302970+Wolfofwaiks@users.noreply.github.com>
2024-05-23 17:00:19 +0000

    built director table

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,9 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def director 
+    render({ :template => "misc_templates/director"})
+
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/directors", { :controller => "misc", :action => "director" })
 end
