9fb3c1bf5fd114d7f62ccd1e249b7f6e0eb8d00f
Wolfofwaiks <165302970+Wolfofwaiks@users.noreply.github.com>
2024-05-23 18:22:30 +0000

    working on filmography table

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -7,4 +7,14 @@ class MiscController < ApplicationController
     render({ :template => "misc_templates/director"})
 
   end 
+
+  def show 
+    the_id = params.fetch("the_id")
+
+    matching_records = Director.where({ :id => the_id })
+
+    @the_director = matching_records.at(0)
+
+    render({ :template => "misc_templates/show"})
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
   get("/directors", { :controller => "misc", :action => "director" })
+
+  get("/directors/:the_id", { :controller => "misc", :action => "show" })
 end
